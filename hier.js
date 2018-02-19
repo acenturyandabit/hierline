cb3aaa32d5b1579c40cff109cd766daa21ba2852
acenturyandabit <steeven.liu2@gmail.com>
2018-02-19 14:02:59 +1000

    date removal enabled

--- hier.js
+++ hier.js
@@ -1,22 +1,24 @@
-var count=0;
-function dataItem(p, name,id) { //initialiser for dataItem
+var count = 0;
+function dataItem(p, name, id) { //initialiser for dataItem
 	//self properties
-	if (!id)this.id = Date.now()*10+count; //for deleting nodes i need strictly unique node ID's - this should generate unique ID's even between runs
-	else this.id=id;
+	if (!id)
+		this.id = Date.now() * 10 + count; //for deleting nodes i need strictly unique node ID's - this should generate unique ID's even between runs
+	else
+		this.id = id;
 	count++;
 	this.name = name;
 	this.longdesc = "";
 	//div for the big block displays; just keeping it on hand. Might take up a ton of memory but we'll see
-	
-	
+
+
 	//cloning, adding events etc
 	this.div = $("#item_template")[0].cloneNode(true); //clone the template
 	this.div.classList.add("activeBlocks");
-	this.div.children[0].children[0].addEventListener("change",finishEdit);
-	this.div.children[2].addEventListener("change",longdescedit);
-	this.div.children[1].addEventListener("change",dateEdit);
-	this.div.children[0].children[2].addEventListener("click",reanchor);
-	this.div.children[0].children[1].addEventListener("click",deleteNode);
+	this.div.children[0].children[0].addEventListener("change", finishEdit);
+	this.div.children[2].addEventListener("change", longdescedit);
+	this.div.children[1].addEventListener("change", dateEdit);
+	this.div.children[0].children[2].addEventListener("click", reanchor);
+	this.div.children[0].children[1].addEventListener("click", deleteNode);
 	this.div.id = "d_" + this.id;
 	this.div.children[0].children[0].value = this.name; //set heading inside div to my name
 	//record the parent element and all the children for the node; just cos
@@ -29,9 +31,9 @@ function dataItem(p, name,id) { //initialiser for dataItem
 			return index;
 		}
 	}
-	this.setlongdesc=function(ld){
-		this.longdesc=ld;
-		this.div.children[2].value=this.longdesc;
+	this.setlongdesc = function (ld) {
+		this.longdesc = ld;
+		this.div.children[2].value = this.longdesc;
 	}
 	/*
 	javascript is kinda funny because there are some strange distinctions between an object and a reference. For example, if i do:
@@ -49,10 +51,16 @@ function dataItem(p, name,id) { //initialiser for dataItem
 	 */
 	//timeline
 	this.taskDate = undefined;
-	this.setDate= function(date){
-		if (!this.taskDate) this.taskDate=new Date();
-		this.taskDate.setTime(date);
-		this.div.children[1].value=this.taskDate.toISOString().split("T")[0];
+	this.setDate = function (date) {
+		if (!date)
+			this.taskDate = undefined;
+		else {
+			if (!this.taskDate)
+				this.taskDate = new Date();
+			this.taskDate.setTime(date);
+			this.div.children[1].value = this.taskDate.toISOString().split("T")[0];
+		}
+		drawTimeline();
 	}
 	//some functions which i'm sure will be useful
 	this.contains = function (item) { //does this node contain the node specified?
@@ -78,39 +86,40 @@ function dataItem(p, name,id) { //initialiser for dataItem
 
 }
 var nodes = []; //store all nodes in an array so we can quickly access them
-function makeNode(parent, name,id) {
-	var p = new dataItem(parent, name,id);
+function makeNode(parent, name, id) {
+	var p = new dataItem(parent, name, id);
 	nodes.push(p);
 	return p;
 }
 
-function getNode(id){
-	for (var i of nodes){
-		if (i.id==id)return i;
-	}//if none found then return nothing
+function getNode(id) {
+	for (var i of nodes) {
+		if (i.id == id)
+			return i;
+	} //if none found then return nothing
 	return undefined;
 }
 
 function selectNode(e) {
-	var nid=e.path[0].id.split("_")[1];
-	if (anchorID!=-1){
-		var sNode=getNode(anchorID);
-		if (sNode.contains(getNode(nid))){
+	var nid = e.path[0].id.split("_")[1];
+	if (anchorID != -1) {
+		var sNode = getNode(anchorID);
+		if (sNode.contains(getNode(nid))) {
 			$("#status").html("Cannot anchor node on its children!");
-			anchorID=-1;
+			anchorID = -1;
 			return;
 		}
-		
-		sNode.parent.children.splice(getNode(anchorID).parent.children.indexOf(getNode(anchorID)),1);
-		sNode.parent=getNode(nid);
-		sNode.siblings=getNode(nid).children;
+
+		sNode.parent.children.splice(getNode(anchorID).parent.children.indexOf(getNode(anchorID)), 1);
+		sNode.parent = getNode(nid);
+		sNode.siblings = getNode(nid).children;
 		getNode(nid).children.push(sNode);
 		this.siblings = this.parent.children;
-		anchorID=-1;
+		anchorID = -1;
 		$("#status").html("Select new anchor node");
 	}
 	drawHierarchy(getNode(nid));
-	currentNode=getNode(nid);
+	currentNode = getNode(nid);
 }
 
 function finishEdit(e) {
@@ -119,25 +128,25 @@ function finishEdit(e) {
 	drawHierarchy(currentNode);
 }
 
-function longdescedit(e){
+function longdescedit(e) {
 	var node_id = e.currentTarget.parentElement.id.split("_")[1];
-	getNode(node_id).longdesc = e.currentTarget.value;	
+	getNode(node_id).longdesc = e.currentTarget.value;
 }
-function dateEdit(e){
+function dateEdit(e) {
 	var node_id = e.currentTarget.parentElement.id.split("_")[1];
-	var t=new Date(e.currentTarget.value);
-	getNode(node_id).setDate(t.valueOf());	
+	var t = new Date(e.currentTarget.value);
+	getNode(node_id).setDate(t.valueOf());
 }
-var anchorID=-1;
+var anchorID = -1;
 function reanchor(e) {
 	$("#status").html("Select new anchor node");
-	anchorID=e.currentTarget.parentElement.parentElement.id.split("_")[1];
+	anchorID = e.currentTarget.parentElement.parentElement.id.split("_")[1];
 }
 function deleteNode(e) {
-	var div_to_delete=e.currentTarget.parentElement.parentElement;
-	var deleteID=div_to_delete.id.split("_")[1];
-	if (getNode(deleteID)==currentNode) currentNode=getNode(deleteID).parent;
-	getNode(deleteID).parent.children.splice(getNode(deleteID).parent.children.indexOf(getNode(deleteID)),1);
+	var div_to_delete = e.currentTarget.parentElement.parentElement;
+	var deleteID = div_to_delete.id.split("_")[1];
+	if (getNode(deleteID) == currentNode)
+		currentNode = getNode(deleteID).parent;
+	getNode(deleteID).parent.children.splice(getNode(deleteID).parent.children.indexOf(getNode(deleteID)), 1);
 	drawHierarchy(currentNode);
 }
-
